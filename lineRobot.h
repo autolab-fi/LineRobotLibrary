ce606e6c7463d4a55a4789f9178772d574f65643
ShulcN <nosovaleksandrse@gmail.com>
2024-01-29 13:28:56 +0300

    decreased degrees-encoder

--- lineRobot.h
+++ lineRobot.h
@@ -57,7 +57,7 @@ public:
         k=0.25;
         radius_wheel =wheel_radius/100;
         distance_between_wheel_and_center = distance_between_wheels/200;
-        encoder_degrees_optimal = 2.85;
+        encoder_degrees_optimal = 2.75;
         ki_rot = 0.0001;
         kd_rot = 0.00001;
         kp_rot = 0.001;
