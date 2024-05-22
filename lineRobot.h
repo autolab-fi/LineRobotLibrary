7d7766dc92ce395953c87d6d5d512c6055b6a623
ShulcN <nosovaleksandrse@gmail.com>
2024-05-22 19:20:23 +0300

    decreased coefficient for forward movement

--- lineRobot.h
+++ lineRobot.h
@@ -53,7 +53,7 @@ public:
         pinMode(in4,OUTPUT);
 	    encRight.attachHalfQuad(18, 19);
 
-        k=0.6;
+        k=0.2;
         radius_wheel =wheel_radius/100;
         distance_between_wheel_and_center = distance_between_wheels/200;
         encoder_degrees_optimal = 2.96;
