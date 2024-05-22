11b2a39ed34dd18b1bc887b54e0ef11cbc5a3d97
ShulcN <nosovaleksandrse@gmail.com>
2024-05-22 20:18:07 +0300

    increased moving coefficient

--- lineRobot.h
+++ lineRobot.h
@@ -53,7 +53,7 @@ public:
         pinMode(in4,OUTPUT);
 	    encRight.attachHalfQuad(18, 19);
 
-        k=0.26;
+        k=0.39;
         radius_wheel =wheel_radius/100;
         distance_between_wheel_and_center = distance_between_wheels/200;
         encoder_degrees_optimal = 3.28;
